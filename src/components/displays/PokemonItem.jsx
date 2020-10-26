fa3b44b3258aedc1eca6c06bbdfd40b5dea4e2ed
katiePDX <61799774+katiepdx@users.noreply.github.com>
2020-10-26 14:19:51 -0700

    add PokemonList snapshot test

--- src/components/displays/PokemonItem.jsx
+++ src/components/displays/PokemonItem.jsx
@@ -3,7 +3,7 @@ import propTypes from 'prop-types'
 
 export default function PokemonItem({ name, image }) {
   return (
-    <div>
+    <div key={name}>
       <p>{name}</p>
       <p>{image}</p>
     </div>
--- src/components/displays/PokemonList.jsx
+++ src/components/displays/PokemonList.jsx
@@ -1,10 +1,20 @@
 // PokemonList display component goes here - list name and image for each PokemonItem
 import React from 'react'
+import propTypes from 'prop-types'
+import PokemonItem from './PokemonItem'
 
-export default function PokemonList() {
-  return (
-    <div>
-      PokemonList page
-    </div>
-  )
+export default function PokemonList({ pokemonData }) {
+  return pokemonData.map(pokemon => (
+    <>
+      {/* Spread pokemon props */}
+      <PokemonItem key={pokemon._id} {...pokemon} />
+    </>
+  ))
+}
+
+PokemonItem.propTypes = {
+  pokemonData: propTypes.arrayOf(propTypes.shape({
+    name: propTypes.string.isRequired,
+    image: propTypes.string.isRequired
+  }))
 }
